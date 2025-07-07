a805d878e4b14a38617019c66a98031488122ac1
Steven Morad <smorad@um.edu.mo>
2025-07-07 10:20:29 +0800

    Bump release to 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="popgym-arcade",
-    version="0.0.2",
+    version="0.0.3",
     author="Wang Zekang, He Zhe, Steven Morad",
     author_email="",
     description="POMDP Arcade Environments on the GPU",
