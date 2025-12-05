07194274cbeca22f593e14b05620e8a8ba45f336
Steven Morad <stevenmorad@gmail.com>
2025-12-05 15:19:01 +0800

    Split tests further (#22)

--- tests/test_step_envs.py
+++ tests/test_step_envs.py
@@ -8,15 +8,6 @@ from popgym_arcade.registration import REGISTERED_ENVIRONMENTS
 @pytest.mark.parametrize("env_name", REGISTERED_ENVIRONMENTS)
 @pytest.mark.parametrize("partial", [False, True])
 @pytest.mark.parametrize("obs_size", [128, 256])
-def test_make(env_name, partial, obs_size):
-    env, env_params = popgym_arcade.make(
-        env_name, partial_obs=partial, obs_size=obs_size
-    )
-
-
-@pytest.mark.parametrize("env_name", REGISTERED_ENVIRONMENTS)
-@pytest.mark.parametrize("partial", [False, True])
-@pytest.mark.parametrize("obs_size", [128, 256])
 def test_reset_and_step_short(env_name, partial, obs_size):
     env, env_params = popgym_arcade.make(
         env_name, partial_obs=partial, obs_size=obs_size
